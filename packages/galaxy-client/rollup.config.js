5eca0d153e11c9b461bf8e651bef9e5c077c360d
Jake.C.Xiao <Jake.C.Xiao@newegg.com>
2018-04-25 23:55:54 +0800

    搭建开发环境

--- packages/galaxy-client/rollup.config.js
+++ packages/galaxy-client/rollup.config.js
@@ -1,16 +1,33 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import uglify from 'rollup-plugin-uglify';
 
-export default {
+export const devConfig = {
     input: 'src/main.ts',
     output: {
         file: 'dist/galaxy.js',
-        format: 'iife'
+        format: 'iife',
+        sourcemap: true
     },
     plugins: [
         resolve(),
         commonjs(),
         typescript()
+    ]
+};
+
+export const releaseConfig = {
+    input: 'src/main.ts',
+    output: {
+        file: 'dist/galaxy.min.js',
+        format: 'iife',
+        sourcemap: 'inline'
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        typescript(),
+        uglify()
     ]
 }

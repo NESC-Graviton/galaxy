c5848a14d08404faf32812180e2ad2c19ddf807b
Jake.C.Xiao <Jake.C.Xiao@newegg.com>
2018-05-08 21:28:38 +0800

    demo

--- packages/galaxy-client/src/_trackPageview.ts
+++ packages/galaxy-client/src/_trackPageview.ts
@@ -1,3 +1,22 @@
 export function _trackPageview() {
-    console.log('Hello World');
+    const params = {} as any;
+
+    if (document) {
+        params.domain = document.domain || '';
+        params.url = document.URL || '';
+        params.title = document.title || '';
+        params.referrer = document.referrer || '';
+    }
+
+    if (window && window.screen) {
+        params.sh = window.screen.height || 0;
+        params.sw = window.screen.width || 0;
+        params.cd = window.screen.colorDepth || 0;
+    }
+
+    if (navigator) {
+        params.lang = navigator.language || '';
+    }
+
+    console.log(JSON.stringify(params));
 }

fe86288e2056ebe000e2c705e67509d79df81989
Jake.C.Xiao <Jake.C.Xiao@newegg.com>
2018-05-16 22:36:08 +0800

    重构

--- packages/galaxy-client/src/entities/EventDescriptor.ts
+++
@@ -1,8 +0,0 @@
-export interface EventDescriptor {
-    category?: 'global' | 'default' | string;
-    action: string;
-    userId?: string;
-    time?: number;
-    pageUrl?: string;
-    payload?: any;
-}
--- packages/galaxy-client/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { EventDescriptor } from 'entities';
-import { startTrack } from './track/index';
-
-declare let _gaq: EventDescriptor[];
-_gaq = _gaq || [];
-
-startTrack(_gaq);

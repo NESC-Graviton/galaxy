571b9690d9202756bcd16c47ef9020ecce5227c1
Jake.C.Xiao <Jake.C.Xiao@newegg.com>
2018-05-22 11:16:57 +0800

    完善发送方式

--- packages/galaxy-sdk/src/core/reporter.ts
+++ packages/galaxy-sdk/src/core/reporter.ts
@@ -1,21 +1,41 @@
 import { EventDescriptor } from 'entities';
 import { config } from './config';
+import { stringify } from 'query-string';
+
+export function report(event: EventDescriptor) {
+    // 优先使用sendBeacon
+    if (window.navigator.sendBeacon) {
+        sendByBeacon(event);
+    } else {
+        sendByImg(event);
+    }
+}
+
+function sendByBeacon(event: EventDescriptor) {
+    const reportUrl = getReportUrl();
+    window.navigator.sendBeacon(reportUrl, new FormData(event as any));
+}
+
+function sendByImg(event: EventDescriptor) {
+    const img = new Image(1, 1);
+    img.src = getSrcUrl(event);
+}
 
 function getReportUrl() {
     return `${window.location.protocol}//${config.reportUrl}`;
 }
 
-function makeRndString() {
-    return `${new Date().valueOf()}_${Math.random() * 10000}`;
+function getSrcUrl(event: EventDescriptor) {
+    // const rndKey = makeRndString();
+    const queryString = stringify(
+        {
+            ...event,
+            payload: JSON.stringify(event.payload)
+        }
+    );
+    return `${getReportUrl()}/log.gif?${queryString}`;
 }
 
-export function report(event: EventDescriptor) {
-    const reportUrl = getReportUrl();
-    // 优先使用sendBeacon
-    if (window.navigator.sendBeacon) {
-        return window.navigator.sendBeacon(reportUrl, new FormData(event as any));
-    }
-    const rndKey = `report_img_${makeRndString()}`;
-    const img = new Image(1, 1);
-    img.src = reportUrl;
-}
+// function makeRndString() {
+//     return `${new Date().valueOf()}_${Math.random() * 10000}`;
+// }
